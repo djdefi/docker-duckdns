e4cc31d0d3723cfb4335f2416e6754c073521def
aptalca <thedjinn@yahoo.com>
2015-06-25 17:18:10 -0400

    Update duck.sh

--- duck.sh
+++ duck.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
 
+if [! -z "$SUBDOMAIN" ]; then
+  RESPONSE=`curl -s "https://www.duckdns.org/update?domains=$SUBDOMAIN&token=$TOKEN&ip="`
+  if [ "$RESPONSE" = "OK" ]; then
+  echo "Your IP was updated at "$(date)
+  else
+  echo "Tried to use subdomain and token from environment values but something went wrong, check your settings  "$(date)
+  fi
+
+elif [ -f /config/duck.conf ]; then
+  echo "duck.conf found! Using old school method to update IP. Please consider passing subdomain and token as environment values in the future as the duck.conf method is depracated."
   tr -d '\r' < /config/duck.conf > /tmp/duck.conf
-
   . /tmp/duck.conf
-
-  if [ -z "$DOMAINS" ]; then
-    echo "DOMAINS must be defined in duck.conf"
-    exit 1
-  elif [ "$DOMAINS" = "yourdomain" ]; then
-    echo "Please enter your domain in duck.conf"
-    exit 1
-  fi
-
-  if [ -z "$TOKEN" ]; then
-    echo "TOKEN must be defined in duck.conf"
-    exit 1
-  elif [ "$TOKEN" = "yourtoken" ]; then
-    echo "Please enter your token in duck.conf"
-    exit 1
-  fi
-  
   RESPONSE=`curl -s "https://www.duckdns.org/update?domains=$DOMAINS&token=$TOKEN&ip="`
   if [ "$RESPONSE" = "OK" ]; then
   echo "Your IP was updated at "$(date)
   else
-  echo "Something went wrong, check your settings  "$(date)
+  echo "Tried to use existing duck.conf file but something went wrong, check your settings  "$(date)
   fi
+
+else
+  echo "Please enter your subdomain and token as environment variables in your docker run command. See info box for details."
+
+fi
