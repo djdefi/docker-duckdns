f5166325590a5cea044a90b6489bc7a86559cc7b
aptalca <thedjinn@yahoo.com>
2015-08-05 12:59:09 -0400

    Update duck.sh

--- duck.sh
+++ duck.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 
-if [ ! -z "$SUBDOMAIN" ]; then
-  RESPONSE=`curl -s "https://www.duckdns.org/update?domains=$SUBDOMAIN&token=$TOKEN&ip="`
-  if [ "$RESPONSE" = "OK" ]; then
-  echo "Your IP was updated at "$(date)
-  else
-  echo "Tried to use subdomain and token from environment values but something went wrong, check your settings  "$(date)
-  fi
 
-elif [ -f /config/duck.conf ]; then
-  echo "duck.conf found! Using old school method to update IP. Please consider passing subdomain and token as environment values in the future as the duck.conf method is deprecated."
-  tr -d '\r' < /config/duck.conf > /tmp/duck.conf
-  . /tmp/duck.conf
-  RESPONSE=`curl -s "https://www.duckdns.org/update?domains=$DOMAINS&token=$TOKEN&ip="`
+if [ -f /config/duck.new.conf ]; then
+  tr -d '\r' < /config/duck.new.conf > /tmp/duck.new.conf
+  . /tmp/duck.new.conf
+  RESPONSE=`curl -s "https://www.duckdns.org/update?domains=$SUBDOMAINS&token=$TOKEN&ip="`
   if [ "$RESPONSE" = "OK" ]; then
   echo "Your IP was updated at "$(date)
   else
-  echo "Tried to use existing duck.conf file but something went wrong, check your settings  "$(date)
+  echo "Something went wrong, please check your settings  "$(date)
   fi
 
 else
