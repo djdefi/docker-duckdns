f3464004b4d387792850c73d4d13c565e88a7ed8
aptalca <thedjinn@yahoo.com>
2015-08-05 13:08:49 -0400

    Update duck.sh

--- duck.sh
+++ duck.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 if [ -f /config/duck.new.conf ]; then
   tr -d '\r' < /config/duck.new.conf > /tmp/duck.new.conf
   . /tmp/duck.new.conf
@@ -10,8 +9,6 @@ if [ -f /config/duck.new.conf ]; then
   else
   echo "Something went wrong, please check your settings  "$(date)
   fi
-
 else
   echo "Please enter your subdomain and token as environment variables in your docker run command. See info box for details."
-
 fi
