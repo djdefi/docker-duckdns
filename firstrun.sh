df324646811eb590923d83edac8cb33b480317f4
aptalca <thedjinn@yahoo.com>
2015-08-05 12:53:53 -0400

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-
+  
   #Check to make sure the subdomain and token are set
-  if [ -z "SUBDOMAIN" ]; then
-    echo "Please pass both your subdomain and token as environment variables in your docker run command. See docker info for more details."
-  elif [ -z "TOKEN" ]; then
-    echo "Please pass both your subdomain and token as environment variables in your docker run command. See docker info for more details."
+  if [ -z "SUBDOMAINS" ] || [ -z "TOKEN" ]; then
+    echo "Please pass both your subdomain(s) and token as environment variables in your docker run command. See docker info for more details."
+    if [ -f /config/duck.conf ]; then
+      echo "Retrieving variables from old duck.conf"
+      tr -d '\r' < /config/duck.conf > /tmp/duck.conf
+      . /tmp/duck.conf
+      rm /config/duck.new.conf
+      echo -e "SUBDOMAINS=$DOMAINS \nTOKEN=$TOKEN \n" >> /config/duck.new.conf
+    fi
   else
     echo "Retrieving subdomain and token from the environment variables"
+    rm /config/duck.new.conf
+    echo -e "SUBDOMAINS=$SUBDOMAINS \nTOKEN=$TOKEN \n" >> /config/duck.new.conf
   fi
   
   #Get docker env timezone and set system timezone
